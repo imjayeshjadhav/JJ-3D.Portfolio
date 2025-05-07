a12c8e7bf53f240fbdf567b86d1c4bcadf53819c
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-05-07 19:15:57 +0530

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Hero from "./sections/Hero"
+
 const App = () => {
   return (
-    <main>
-        <div>
-            <h1 className="text-3xl">Welcome</h1>
-        </div>
-    </main>
+    <>
+        <Hero/>
+    </>
   )
 }
 
